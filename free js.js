33fd9193e2df95cef4b4226cd0215a9dcb965d58
forgetzz <forget.noxa90@gmail.com>
2023-08-10 18:18:56 +0800

    ubah

--- free js.js
+++ free js.js
@@ -1,11 +1,6 @@
-const menu = document.querySelector('.menu-toggle input');
-const ul = document.querySelector('nav ul');
+const menu = document.querySelector(".menu-toggle input");
+const ul = document.querySelector("nav ul");
 
-menu.addEventListener ('click', function () {
-  ul.classList.toggle('slide');
+menu.addEventListener("click", function () {
+  ul.classList.toggle("slide");
 });
-
-const nav = document.querySelector('nav');
-window.addEventListener ('pointerdown', function() {
-  nav.classList.toggle('stiky')
-})
