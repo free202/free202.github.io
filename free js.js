c2fb89f80e98f4a50ae322bd92dfb4d5ef9f6836
forgetzz <forget.noxa90@gmail.com>
2023-08-11 14:37:33 +0800

    tinggal fasility

--- free js.js
+++ free js.js
@@ -4,3 +4,19 @@ const ul = document.querySelector("nav ul");
 menu.addEventListener("click", function () {
   ul.classList.toggle("slide");
 });
+
+var mati = document.querySelector(".loading");
+
+window.addEventListener("load", function () {
+  mati.style.display = "none";
+});
+
+var gambar = document.querySelector(".asa");
+window.addEventListener("load", function () {
+  gambar.style.display = "none";
+});
+
+var tutup = document.querySelector(".backg");
+window.addEventListener("load", function () {
+  tutup.style.display = "none";
+});
